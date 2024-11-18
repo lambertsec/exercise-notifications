17f842ee32b26fee40545722c7759c4e70436160
markl <mark.lambert.wtap@gmail.com>
2024-11-18 18:39:12 -0500

    added test folders and constants for notification types

---
+++ src/main/java/com/exerciseTracker/app/Constants/NotificationType.java
@@ -0,0 +1,8 @@
+package com.exerciseTracker.app.Constants;
+
+public final class NotificationType {
+
+    public static final String ACTIVITY_REMINDER = "activity";
+    public static final String GOAL_ACHIEVEMENT = "goal";
+    public static final String SOCIAL_UPDATE = "social";
+}
---
+++ src/test/java/com/exerciseTracker/app/service/NotificationRepositoryTests.java
@@ -0,0 +1,13 @@
+package com.exerciseTracker.app.service;
+
+import org.junit.jupiter.api.Test;
+
+public class NotificationRepositoryTests {
+
+
+
+
+
+    @Test
+    void
+}
